d824d50d356cc631fb1222fecfa8f8c746a6c172
thinkit <shinkunihei@shinkuniheimatoMacBook-Pro.local>
2014-07-03 16:56:42 +0800

    1. redirect stderr output to file(device only) 2. set uncaught exception handler 3. advice users to send email using alterView

--- UncaughtException/AppDelegate.m
+++ UncaughtException/AppDelegate.m
@@ -13,6 +13,37 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+#ifndef TARGET_IPHONE_SIMULATOR
+    NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
+    if ( [settings boolForKey:@"ExceptionOccuredOnLastRun"])
+    {
+        // reset flag
+        [settings setBool:NO forKey:@"ExceptionOccuredOnLastRun"];
+        [settings synchronize];
+        
+        
+        // show an alert to user
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry"
+                                                            message:@"UncaughtException occured on last run"
+                                                           delegate:self
+                                                  cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+        [alertView addButtonWithTitle:@"Send Email"];
+        [alertView show];
+    }
+    else
+    {
+        NSSetUncaughtExceptionHandler(&myUncaughtExceptionHandler);
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentPath = [paths objectAtIndex:0];
+        NSString *stderrFile = [documentPath stringByAppendingPathComponent:@"stderr.log"];
+        
+        // Redirect stderr output to file
+        freopen([stderrFile cStringUsingEncoding:NSASCIIStringEncoding], "w", stderr);
+    }
+    
+#endif
+  
     return YES;
 }
 							
@@ -42,5 +73,57 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+#pragma mark - UNcaughtExceptionHandler
+
+void myUncaughtExceptionHandler(NSException *exception)
+{
+    NSLog(@"Uncaught exception: %@ \nReason:%@\n User info: %@\n CallStackSymbol:%@\n", exception.name, exception.reason, exception.userInfo, exception.callStackSymbols);
+     NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
+     [settings setBool:YES forKey:@"ExceptionOccuredOnLastRun"];
+     [settings synchronize];
+}
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentPath = [paths objectAtIndex:0];
+    NSString *stderrFile = [documentPath stringByAppendingPathComponent:@"stderr.log"];
+    
+    if (buttonIndex == 1)
+    {
+        // Send email
+        MFMailComposeViewController *mailComposer = [[MFMailComposeViewController alloc] init];
+        mailComposer.mailComposeDelegate = self;
+        [mailComposer setSubject:@"Error Report"];
+        [mailComposer setToRecipients:[NSArray arrayWithObject:@"lisai@thinkit.cn"]];
+        
+        // Attach log file
+        NSData *data = [NSData dataWithContentsOfFile:stderrFile];
+        [mailComposer addAttachmentData:data mimeType:@"Text/XML" fileName:@"stderr.log"];
+        
+        // Compose email body
+        UIDevice *device = [UIDevice currentDevice];
+        NSString *messageBody = [NSString stringWithFormat:@"Device model:%@\n OS:%@\n OS version:%@\n", device.model, device.systemName, device.systemVersion];
+        [mailComposer setMessageBody:messageBody isHTML:NO];
+        
+        [self.window.rootViewController presentViewController:mailComposer animated:YES completion:nil];        
+    }
+    
+    NSSetUncaughtExceptionHandler(&myUncaughtExceptionHandler);
+    
+    // Redirect stderr output to file
+    freopen([stderrFile cStringUsingEncoding:NSASCIIStringEncoding], "w", stderr);
+}
+
+#pragma mark - MFMailComposeViewControllerDelegate
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [self.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
